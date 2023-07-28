869c62938424e29dd15637368b22116d306daa98
Daniele Nicolucci <daniele@nicolucci.eu>
2023-07-28 21:58:20 +0200

    don't use temp file

--- add_static_v6.sh
+++ add_static_v6.sh
@@ -5,14 +5,10 @@ HOSTID=::1984
 
 ###
 
-tmpfile=`mktemp`
-
-ifconfig $IF > $tmpfile
-base=`grep "autoconf secured" $tmpfile | cut -d ' ' -f2 | cut -d ':' -f1-4` # 1-4 => /64
+base=`ifconfig $IF | grep "autoconf secured" | cut -d ' ' -f2 | cut -d ':' -f1-4` # 1-4 => /64
 newip="$base$HOSTID"
+echo -e "Adding $newip:\n"
 ifconfig $IF inet6 $newip
 
 # proof
 ifconfig $IF
-
-rm $tmpfile
